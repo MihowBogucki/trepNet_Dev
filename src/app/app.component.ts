326783437ef1df6e053bc9da94b6d05235d0ff98
MihowBogucki <mihowguitar@gmail.com>
2018-02-12 07:58:20 +0200

    UI Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Marketplace', component: HomePage },
-      { title: 'Marketplace Chat', component: MarketplaceChatPage },
+      { title: 'Work In Progress', component: MarketplaceChatPage },
       { title: 'Directory', component: DirectoryPage },
       { title: 'Profile', component: ProfilePage },
     ];
